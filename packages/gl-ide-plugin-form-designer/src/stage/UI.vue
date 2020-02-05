da389108d1c4ad3055cdbef6c5d470d1b6ff2589
geelato <geelato@126.com>
2020-02-05 21:51:36 +0800

    update form designer

--- packages/gl-ide-plugin-form-designer/src/stage/UI.vue
+++ packages/gl-ide-plugin-form-designer/src/stage/UI.vue
@@ -4,11 +4,8 @@
     <u-i-item :rows="rows"
               :properties="opts.properties"
               :form="form"
-              :componentRefs="{}"
-              :events="{}"
-              :bindEvents="{}"
               :gutter="opts.layout.gutter"
-              :treeNodes="[]"
+              @updateLayoutRows="updateLayoutRows"
     ></u-i-item>
   </div>
 </template>
@@ -33,7 +30,8 @@
     },
     created() {
       this.rows = this.convertToSrcLayoutRows(this.opts.layout.rows)
-      console.log('geelato-ide >  GlIdePluginFormDesignerStageUI > mounted()', this.opts)
+      console.log('geelato-ide >  GlIdePluginFormDesignerStageUI > mounted() > this.opts:', this.opts)
+      console.log('geelato-ide >  GlIdePluginFormDesignerStageUI > mounted() > this.rows:', this.rows)
     },
     mounted() {
     },
@@ -44,33 +42,101 @@
        * @return {Array}
        */
       convertToSrcLayoutRows(rows) {
+        let that = this
         let newRows = []
         // 转换布局信息
         rows.forEach((row) => {
           let newRow = {
             cols: []
           }
-          for (let i = 0; row.cols.length > i; i += 2) {
-            let labelCol = row.cols[i]
-            let controlCol = row.cols[i + 1]
-            console.log('row.cols>>>', row.cols)
-            let x = 24 / (labelCol.span + controlCol.span)
-            newRow.cols.push({
-              span: labelCol.span + controlCol.span,
-              items: [{
-                fields: [
-                  {
-                    title: labelCol.label,
-                    field: controlCol.field
-                  }
-                ],
-                fieldSpan: {label: labelCol.span * x, control: controlCol.span * x}
-              }]
-            })
+          for (let i = 0; row.cols.length > i;) {
+            let col = row.cols[i]
+            if (col.rows) {
+              newRow.cols.push({
+                span: col.span,
+                rows: that.convertToSrcLayoutRows(col.rows)
+              })
+              i++
+            } else {
+              let labelCol = row.cols[i]
+              let controlCol = row.cols[i + 1]
+              let x = 24 / (labelCol.span + controlCol.span)
+              newRow.cols.push({
+                span: labelCol.span + controlCol.span,
+                // items对应一个格子中，有多组label、control的场景，以支持用户头像等需跨多行展示的场景
+                // 目前items只有一项，只暂不支持用户头像需跨多行的场景
+                items: [{
+                  fields: [
+                    {
+                      title: labelCol.label,
+                      field: controlCol.field
+                    }
+                  ],
+                  fieldSpan: {label: labelCol.span * x, control: controlCol.span * x}
+                }]
+              })
+              i += 2
+            }
           }
           newRows.push(newRow)
         })
         return newRows
+      },
+      //
+      convertToReleaseLayoutRows(rows) {
+        let newRows = []
+        rows.forEach((row) => {
+          let newRow = {
+            cols: []
+          }
+          row.cols.forEach((col) => {
+            if (col.rows) {
+              // 未支持col嵌套rows
+              // this.convertToReleaseLayoutRows(col.rows)
+            } else {
+              col.items.forEach((item) => {
+                if (item.fields && item.fields.length > 0) {
+                  const x = col.span / 24
+                  let fieldSpan = item.fieldSpan
+                  let fieldItem = item.fields[0]
+                  // {span: 4, label: true, field: 'avatar'}
+                  // 第一个有效，只取第一个field，
+                  newRow.cols.push({
+                    span: fieldSpan.label * x,
+                    label: fieldItem.title || true,
+                    field: fieldItem.field
+                  })
+                  newRow.cols.push({
+                    span: fieldSpan.control * x,
+                    field: fieldItem.field
+                  })
+                } else {
+                  const x = col.span / 24
+                  let fieldSpan = item.fieldSpan
+                  newRow.cols.push({
+                    span: fieldSpan.label * x,
+                    label: '',
+                    field: ''
+                  })
+                  newRow.cols.push({
+                    span: fieldSpan.control * x,
+                    field: ''
+                  })
+                }
+              })
+            }
+          })
+          newRows.push(newRow)
+        })
+        console.log('convertToReleaseLayoutRows> after:', newRows)
+        return newRows
+      },
+      updateLayoutRows(updatedRows) {
+        // this.$set(this.opts.layout, 'rows', this.convertToReleaseLayoutRows(updatedRows))
+        this.opts.layout.rows.splice(0, this.opts.layout.rows.length)
+        this.convertToReleaseLayoutRows(updatedRows).forEach((row) => {
+          this.opts.layout.rows.push(row)
+        })
       }
     }
   }
--- packages/gl-ide-plugin-form-designer/src/stage/UIItem.vue
+++ packages/gl-ide-plugin-form-designer/src/stage/UIItem.vue
@@ -6,12 +6,13 @@
         group='layoutItems'
         :sort="true"
         @add="onRowAdd"
-        @end="onEnd"
+        @end="onRowEnd"
         @clone="onRowClone"
         @change="onRowChange"
     >
       <a-row v-for="(row,rowIndex) in rowItems" :gutter="row.gutter||gutter" :key="rowIndex" class="gl-dnd-row-handle">
         <a-col v-for="(col,colIndex) in row.cols" :span="col.span" :offset="col.offset" :key="colIndex" style="">
+          <!--col.items为最后一级。未支持嵌套rows的场景，即不支持col.rows-->
           <a-row v-for="(item,itemIndex) in col.items" :key="itemIndex"
                  class="col">
             <a-col :span="item.fieldSpan.label" class="label">
@@ -25,14 +26,17 @@
                   group='field'
                   :sort="false"
                   @add="onAddControl($event,item)"
-                  @end="onEndControl($event,item)"
+                  @end="onRowEndControl($event,item)"
                   @change="onControlChange($event,item)"
                   @choose="onControlChoose($event,item)"
                   class="gl-dnd-col-wrapper"
               >
-                <gl-control v-if="fieldItem.field" class="gl-dnd-control-handle " v-for="fieldItem in item.fields"
+                <gl-control v-if="fieldItem.field" class="gl-dnd-control-handle "
+                            v-for="(fieldItem,fieldItemIndex) in item.fields"
                             :ref="getProperty(fieldItem.field).gid" :form="form"
-                            :property="getProperty(fieldItem.field)"></gl-control>
+                            :property="getProperty(fieldItem.field)"
+                            :key="fieldItemIndex"
+                ></gl-control>
                 <!--<div v-if="fieldItem.field" class="gl-dnd-control-handle" v-for="fieldItem in item.fields">-->
                 <!--<a-icon :type="getControlType(getProperty(fieldItem.field).control).icon"/>-->
                 <!--{{getControlType(getProperty(fieldItem.field).control).title}}-->
@@ -42,55 +46,24 @@
           </a-row>
         </a-col>
         <div class="gl-dnd-row-toolbar">
-          <!--<a-icon type="close-circle" theme="twoTone" twoToneColor="#f5222d" class="gl-dnd-row-handlex" title="移动行"/>-->
-          <a-icon type="close-circle" theme="twoTone" twoToneColor="#f5222d" @click="removeRow(rowIndex)" title="删除行"/>&nbsp;&nbsp;
+          <a-icon type="close-circle" theme="twoTone" twoToneColor="#f5222d" class="gl-dnd-row-handlex" title="移动行"/>
+          <!--<a-icon type="close-circle" theme="twoTone" twoToneColor="#f5222d" @click="removeRow(rowIndex)" title="删除行"/>&nbsp;&nbsp;-->
         </div>
       </a-row>
       <div v-if="!rowItems||rowItems.length===0" style="text-align: center;padding-top: 1em">
         从左边【布局】栏目中拖动布局行列到该区域，再从【组件】栏目拖动组件到布局行列中。
       </div>
     </gl-draggable>
-    <div v-if="modalVisible">
-      <!--<a-button type="primary" @click="() => modalVisible = true">Vertically centered modal dialog</a-button>-->
-      <a-modal class="gl-card-designer" :title="modalTitle" centered :width=modalWidth v-model="modalVisible"
-               @ok="() => modalVisible = false" @cancel="onCloseModal" okText="保存" cancelText="取消"
-               :maskClosable="false">
-        <component :is="$globalVue.component(currentOpenedCard.meta.component)"
-                   v-bind="currentOpenedCard.bind"></component>
-        <template slot="footer">
-          <div style="text-align: center">
-            <!--<a-button type="primary" @click="saveCardComponent">暂存</a-button>-->
-            <a-button type="danger" @click="onCloseModal">
-              关闭
-            </a-button>
-          </div>
-        </template>
-      </a-modal>
-    </div>
   </div>
 </template>
 
 <script>
   /* eslint-disable no-unused-vars */
-  import EditingFileParser from '../../../../runingtime/EditingFileParser'
-  import Vue from 'vue'
   import controlTypes from '../data/controlTypes'
 
   export default {
     name: "GlIdePluginLayoutStageUIItem",
     props: {
-      componentRefs: {
-        type: Object,
-        required: false
-      },
-      events: {
-        type: Object,
-        required: false
-      },
-      bindEvents: {
-        type: Object,
-        required: false
-      },
       rows: {
         type: Array,
         default() {
@@ -109,12 +82,6 @@
           return {}
         }
       },
-      treeNodes: {
-        type: Array,
-        default() {
-          return []
-        }
-      },
       gutter: {
         type: Number,
         default() {
@@ -124,15 +91,7 @@
     },
     data() {
       return {
-        color: '#FFF',
-        modalTitle: '&nbsp;',
-        modalWidth: (window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth) * .98,
-        currentOpenedCard: {},
-        modalVisible: false,
         rowItems: this.rows,
-        colItems: [],
-        colCards: {},
-        currentCardId: '',
         controlTypes: controlTypes
       }
     },
@@ -140,187 +99,18 @@
     created() {
       this.$gl.bus.$on('gl_ide_plugin_layout__modal_close', this.onClose)
     },
+    destroyed(){
+      this.$gl.bus.$off('gl_ide_plugin_layout__modal_close', this.onClose)
+    },
     mounted() {
-      // console.log('this.props>>>>>>>>>>>>', this.componentRefs)
-      // console.log('this.props>>>>>>>>>>>>', this.rows)
-      // console.log('this.props>>>>>>>>>>>>', this.treeNodes)
-      // console.log('this.props>>>>>>>>>>>>', this.gutter)
-      this.reset()
     },
     methods: {
-      reset() {
-        this.editingFileParser = new EditingFileParser().init(this.$root)
-        // this.initComponentRefs()
-        // this.generateTreeNodeData()
-      },
-      /**
-       * 初始化创建树节点
-       */
-      generateTreeNodeData() {
-        console.log('gl-ide > gl-ide-plugin-item > generateTreeData() > treeNodes:', this.treeNodes)
-        let that = this
-        if (that.treeNodes !== undefined && that.treeNodes.length > 0) {
-          // 已创建，不重复创建
-          console.warn('已创建，不重复创建')
-          return;
-        }
-        that.rows.filter((row) => !!row.cols).forEach((row) => {
-          row.cols.filter((col) => !!col.items).forEach((col) => {
-            // ==========item为卡片内一个组件的配置信息，例如下方所示
-            // {id:'',title: '列表',icon: 'table',component: 'GlTable',bind: {opts: table, query: {}},
-            //   meta: {
-            //     component: 'GlIdePluginTableDesigner',
-            //       title: '列表编辑器',
-            //       objectTree: [{title: '查询栏', path: 'query.mix.properties'}, {title: '工具栏', path: 'toolbar.actions'}]
-            //   }
-            // }
-
-            col.items.forEach((item) => {
-              that.generateObjectTreeNodeAndBindEvent(item)
-            })
-          })
-        })
-        console.log('gl-ide > gl-ide-plugin-item > generateTreeData() > componentRefs:', this.componentRefs)
-      },
-      /**
-       * 初始化组件树中的组件引用
-       */
-      initComponentRefs() {
-        for (let rowIndex in this.rowItems) {
-          let row = this.rowItems[rowIndex]
-          for (let colIndex in row.cols) {
-            for (let colItemIndex in row.cols[colIndex].items) {
-              this.generateComponentRef(row.cols[colIndex].items[colItemIndex])
-            }
-          }
-        }
-      },
-      generateComponentRef(item) {
-        // console.log('gl-ide > gl-ide-plugin-item > generateComponentRef() > item:', item)
-        // console.log('gl-ide > gl-ide-plugin-item > generateComponentRef() > this.$refs:', this.$refs)
-        // console.log('gl-ide > gl-ide-plugin-item > generateComponentRef() > this.$refs[item.id]:', this.$refs[item.id])
-        // this.componentRefs[item.id] = {
-        //   id: item.id,
-        //   component: this.$refs[item.id][0],
-        //   type: item.type,
-        //   bind: item.bind,
-        //   meta: item.meta
-        // }
-      },
-      /**
-       * 创建该组件(treeNodes)下的树节点
-       * @param item 组件配置信息item
-       */
-      generateObjectTreeNodeAndBindEvent(item) {
-
-        let that = this
-        // 如果已存在treeNodes中，则不添加
-        if (that.treeNodes.filter((node) => node.key === item.id).length > 0) {
-          console.warn('gl-ide > gl-ide-plugin-item > generateObjectTreeNodeAndBindEvent() > 已存在treeNodes中，不添加item:', item)
-          return
-        }
-        // 加载每张卡片组件配置cardComponent
-        //  {id: item.id, component: this.$refs[item.id][0], type: item.type, meta: item.meta}
-        let cardComponent = that.componentRefs[item.id]
-        console.log('gl-ide > gl-ide-plugin-item > generateObjectTreeNodeAndBindEvent() > item.id,cardComponent:', item.id, cardComponent, that.componentRefs)
-
-        // console.log('gl-ide > gl-ide-plugin-item > generateObjectTreeNodeAndBindEvent() > cardComponent:', cardComponent)
-        let groups = []
-        if (cardComponent && cardComponent.meta && cardComponent.meta.objectTree) {
-          cardComponent.meta.objectTree.forEach((treeNodeObject) => {
-            // treeNodeObject: {title:xx,path:xx.yy.zz}
-            let childrenNodes = []
-            let childrenObjects = eval('item.bind.opts.' + treeNodeObject.path)
-            console.log('gl-ide > gl-ide-plugin-layout > generateTreeData() > childrenObjects:', cardComponent.title, childrenObjects)
-            if (childrenObjects && typeof childrenObjects === "object") {
-              for (let key in childrenObjects) {
-                let childObj = childrenObjects[key]
-                if (childObj.control) {
-                  // 未设置control值的，可能为form的隐藏属性，这里需过滤掉
-                  // console.log('childObj>', childObj)
-                  childrenNodes.push({
-                    title: childObj.title + ' [' + childObj.control + ']',
-                    // 组件id+组件内的控件id
-                    key: item.id + '_$_' + childObj.gid, // that.$gl.utils.uuid(8),
-                    slots: {
-                      icon: 'link',
-                    }
-                  })
-                  // 基于配置的事件初始化绑定
-                  let componentItem = that.componentRefs[item.id]
-                  let controlComponent = componentItem.component.$_getRefByGid(childObj.gid)
-                  let control = {
-                    gid: childObj.gid,
-                    title: childObj.title,
-                    component: controlComponent
-                  }
-                  if (controlComponent && that.events[childObj.gid]) {
-                    that.editingFileParser.bindEvent(that.bindEvents, control, that.events[childObj.gid])
-                  }
-                }
-              }
-            }
-            groups.push({
-              title: treeNodeObject.title + '[组]',
-              key: that.$gl.utils.uuid(8),
-              disabled: true,
-              slots: {
-                icon: 'folder',
-              },
-              children: childrenNodes
-            })
-            // console.log('gl-ide > gl-ide-plugin-layout > generateTreeData() > component.$refs.query:', eval('item.bind.opts.' + treeNodeObject.path))
-          })
-        }
-
-        that.treeNodes.push({
-          title: item.title,
-          key: item.id,
-          slots: {
-            icon: item.icon,
-          },
-          children: groups
-        })
-      },
-      /**
-       * 移除objectTree相应的节点
-       * @param item 组件配置信息item
-       */
-      removeObjectTreeNode(item) {
-        let that = this
-        that.treeNodes.forEach((node, index) => {
-          if (node.key === item.id) {
-            that.treeNodes.splice(index, 1)
-          }
-        })
-      },
-      saveCardComponent(e) {
-        console.log('saveCardComponent>', e)
-      },
-
-      onCloseModal(e) {
-        this.modalVisible = false
-        this.onCardReload(this.currentOpenedCard)
-      },
-      onCardOpen(col, item, index) {
-        if (item.meta) {
-          this.modalTitle = item.meta.title
-          this.currentOpenedCard = item
-        }
-        this.modalVisible = true
-      },
-      getCardConfig(cardId) {
-        return this.componentRefs[cardId]
-      },
-      getCardComponent(cardId) {
-        let card = this.getCardConfig(cardId)
-        return Vue.component(card.type)
-      },
-      onEnd: function (args) {
-        console.log('gl-ide-plugin-layout > stage > onEnd: ', args, this.properties)
+      onRowEnd: function (args) {
+        console.log('gl-ide-plugin-layout > stage > onRowEnd: ', args, this.properties)
       },
       onRowAdd: function (args) {
         console.log('gl-ide-plugin-layout > stage > onRowAdd: ', args, this.rowItems)
+        this.$emit('updateLayoutRows', this.rowItems)
       },
       removeRow(rowIndex) {
         let that = this
@@ -365,12 +155,14 @@
         // console.log('gl-ide-plugin-layout > stage > onAddControl() > this.refs: ', this.$refs)
         // console.log('gl-ide-plugin-layout > stage > onAddControl() > this.componentRefs: ', this.componentRefs)
       },
-      onEndControl: function (e, fieldItems) {
-        console.log('gl-ide-plugin-form-designer > stage > onEndControl() > e: ', e)
-        console.log('gl-ide-plugin-form-designer > stage > onEndControl() > fieldItems: ', fieldItems)
-        let item = this.getCurrentControlItem(e, fieldItems)
-        console.log('gl-ide-plugin-form-designer > stage > onEndControl() > item: ', item)
 
+      onRowEndControl: function (e, fieldItems) {
+        // console.log('gl-ide-plugin-form-designer > stage > onRowEndControl() > e: ', e)
+        // console.log('gl-ide-plugin-form-designer > stage > onRowEndControl() > fieldItems: ', fieldItems)
+        let item = this.getCurrentControlItem(e, fieldItems)
+        // console.log('gl-ide-plugin-form-designer > stage > onRowEndControl() > item: ', item)
+        console.log('gl-ide-plugin-form-designer > stage > onRowEndControl() > rowItems: ', this.rowItems)
+        this.$emit('updateLayoutRows', this.rowItems)
         // let item = col.items[col.items.length === e.newIndex && e.newIndex > 0 ? e.newIndex - 1 : e.newIndex]
         // this.generateComponentRef(item)
         // this.generateObjectTreeNodeAndBindEvent(item)
@@ -383,7 +175,7 @@
         console.log('gl-ide-plugin-form-designer > stage > onControlChange() > fieldItems: ', fieldItems)
       },
       onControlChoose(e, fieldItems) {
-        console.log('gl-ide-plugin-layout > stage > onControlChoose: ', e)
+        // console.log('gl-ide-plugin-layout > stage > onControlChoose: ', e)
         let item = this.getCurrentControlItem(e, fieldItems)
         this.$gl.bus.$emit('gl-ide-plugin-form-designer.stage.fieldSelect', this.getProperty(item.field))
       },
@@ -434,7 +226,7 @@
 
       },
       onClose() {
-        console.log('close>>>>>>>>>>>>>>>>')
+        // console.log('close>>>>>>>>>>>>>>>>')
         // removePropertiesInRow(rowIndex) {
         //   const that = this
         //   const row = that.rowItems[rowIndex]
@@ -443,10 +235,10 @@
         //     delete that.properties[col.field]
         //   })
         // },
-        const that = this
-        that.rowItems.forEach(function (rowItem) {
-          console.log('that.rows', that.rows)
-        })
+        // const that = this
+        // that.rowItems.forEach(function (rowItem) {
+        //   console.log('that.rows', that.rows)
+        // })
       }
     }
   }
